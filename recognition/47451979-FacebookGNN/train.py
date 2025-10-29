15541c7dea12a55614ea77e14b0a4be6901e503c
connor-203 <chthewilliam2357@gmail.com>
2025-10-29 21:44:03 +1000

    Now calculates validation loss, also moved predicting code to predict.py

--- recognition/47451979-FacebookGNN/train.py
+++ recognition/47451979-FacebookGNN/train.py
@@ -1,78 +1,50 @@
 from dataset import data, train_empty, test_empty, device
 from modules import GNN
-import matplotlib as plt
-import numpy as np
+import matplotlib.pyplot as plt
 import torch
-import umap
-import umap.plot
+
 
 # Note, umap.plot requires pandas matplotlib datashader
 # bokeh holoviews scikit-image and colorcet to be installed
 
+# Whether you want to see graphs at the end.
+plotting = True
+
 # This bit of code is fairly closely based on the code from medium, I
 # have suspicions though that medium got it from Kaggle. In any case,
 # this exact block of code is used for quite a few machine learning problems.
-plotting = True
 model = GNN().to(device)
-error = torch.nn.CrossEntropyLoss().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 
 times = []
-losses = []
+losses_train = []
+losses_val = []
 
-model.train()
-for epoch in range(1000):
+for epoch in range(2000):
+    model.train()
     optimizer.zero_grad()
     pred = model(data.x, data.edge_index)
-    loss = error(pred[train_empty], data.y[train_empty])
+    loss = criterion(pred[train_empty], data.y[train_empty])
     loss.backward()
     optimizer.step()
 
     times.append(epoch)
-    losses.append(loss)
+    losses_train.append(loss.item())
+
+    # Calculate validation loss. Code loosely based on something found on stackoverflow.
+    model.eval()
+    pred_val = model(data.x, data.edge_index)
+    loss_val = criterion(pred_val[test_empty], data.y[test_empty])
+    losses_val.append(loss_val.item())
 
     if (epoch+1) % 100 == 0:
-        print(f"Epoch: {epoch + 1:03d}, Loss: {loss:.4f}")
-        
-# This little block of code is also based off the code from medium.com. Any port in storm I suppose.
-model.eval()
-result = model(data.x, data.edge_index) # May be able to get rid of the equals bit, just do model(xyz)
-pred = result.argmax(dim=1) # I don't know what this does yet.
-test_correct = pred[test_empty] == data.y[test_empty]
-test_acc = int(test_correct.sum()) / int(test_empty.sum())
-print(test_acc)
+        print(f"Epoch: {epoch + 1:03d}, Training Loss: {loss:.4f}, Validation Loss: {loss_val:.4f}")
 
-finish_model = model
-# First test had an accuracy of 56%, you started somewhere Connor.
-
-
-# print(pred)
-# print(pred.shape)
-# print(test_empty)
-# print(test_empty.shape)
-
-new_label = []
-for i in data.y:
-    if i == 0:
-        new_label.append("Company")
-    elif i == 1:
-        new_label.append("Government")
-    elif i == 2:
-        new_label.append("Politician")
-    elif i == 3:
-        new_label.append("TV Show")
-
-    
-# print(data.y)
-# print(new_label)
-if plotting:
-    # UMAP doesn't have GPU support so the data must be transferred to the CPU.
-    mapper = umap.UMAP().fit(data.x.to('cpu'))
-    plotted = umap.plot.points(mapper, labels=np.array(new_label))
-    # connect = umap.plot.connectivity(mapper, show_points=True)
-
-    # Hammer_bundle operation requires dask and scikit-image
-    connect = umap.plot.connectivity(mapper, edge_bundling='hammer')
-    umap.plot.show(plotted)
-
-print("Success")
+plt.plot(times, losses_train, label="Training Loss")
+plt.plot(times, losses_val, label="Validation Loss")
+plt.title("Training and Validation Losses")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("assets/Training and Validation Losses for a GNN.png")
