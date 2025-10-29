ca5e307b667e20fd8c09ae767402e3c28d90ced6
connor-203 <chthewilliam2357@gmail.com>
2025-10-29 17:53:03 +1000

    Added loss tracking, moved tensors to GPU, and can now toggle plots.

--- recognition/47451979-FacebookGNN/train.py
+++ recognition/47451979-FacebookGNN/train.py
@@ -1,5 +1,6 @@
-from dataset import data, train_empty, test_empty
+from dataset import data, train_empty, test_empty, device
 from modules import GNN
+import matplotlib as plt
 import numpy as np
 import torch
 import umap
@@ -11,9 +12,13 @@ import umap.plot
 # This bit of code is fairly closely based on the code from medium, I
 # have suspicions though that medium got it from Kaggle. In any case,
 # this exact block of code is used for quite a few machine learning problems.
-model = GNN()
-error = torch.nn.CrossEntropyLoss()
+plotting = True
+model = GNN().to(device)
+error = torch.nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+times = []
+losses = []
 
 model.train()
 for epoch in range(1000):
@@ -22,6 +27,9 @@ for epoch in range(1000):
     loss = error(pred[train_empty], data.y[train_empty])
     loss.backward()
     optimizer.step()
+
+    times.append(epoch)
+    losses.append(loss)
 
     if (epoch+1) % 100 == 0:
         print(f"Epoch: {epoch + 1:03d}, Loss: {loss:.4f}")
@@ -57,13 +65,14 @@ for i in data.y:
     
 # print(data.y)
 # print(new_label)
+if plotting:
+    # UMAP doesn't have GPU support so the data must be transferred to the CPU.
+    mapper = umap.UMAP().fit(data.x.to('cpu'))
+    plotted = umap.plot.points(mapper, labels=np.array(new_label))
+    # connect = umap.plot.connectivity(mapper, show_points=True)
 
-mapper = umap.UMAP().fit(data.x)
-plotted = umap.plot.points(mapper, labels=np.array(new_label))
-# connect = umap.plot.connectivity(mapper, show_points=True)
-
-# hammer_bundle operation requires dask and scikit-image
-connect = umap.plot.connectivity(mapper, edge_bundling='hammer')
-umap.plot.show(plotted)
+    # Hammer_bundle operation requires dask and scikit-image
+    connect = umap.plot.connectivity(mapper, edge_bundling='hammer')
+    umap.plot.show(plotted)
 
 print("Success")
