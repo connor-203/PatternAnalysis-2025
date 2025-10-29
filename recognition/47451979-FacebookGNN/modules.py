410e0b1fb302f530b0c0cbbf452d5585b630a890
connor-203 <chthewilliam2357@gmail.com>
2025-10-29 21:43:32 +1000

    Update modules.py by removing last layer

--- recognition/47451979-FacebookGNN/modules.py
+++ recognition/47451979-FacebookGNN/modules.py
@@ -4,18 +4,20 @@ from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
 from dataset import data
 
+# Using standard structure for graph neural networks,
+# this is based on the code from Kaggle but with one
+# extra layer and a different random seed.
 class GNN(nn.Module):
     def __init__(self):
         super().__init__()
-        torch.manual_seed(42) # I'm not too sure how this line works, I'll play around with it.
-        self.conv1 = GCNConv(data.num_features, 64) # I want to try some different values here.
-        self.conv2 = GCNConv(64, 32)
+        torch.manual_seed(42) # Random seed.
+        self.conv1 = GCNConv(data.num_features, 64)
+        self.conv2 = GCNConv(64, 64)
+        self.conv3 = GCNConv(64, 32)
+        self.conv4 = GCNConv(32, 16)
+        self.conv5 = GCNConv(16, 8)
+        self.conv6 = GCNConv(8, torch.unique(data.y).size(0))
 
-        self.conv3 = GCNConv(32, 16)
-        self.conv4 = GCNConv(16, 8) # I want to try some different values here.
-        self.conv5 = GCNConv(8, 4) # I want to try some different values here.
-        self.conv6 = GCNConv(4, torch.unique(data.y).size(0)) # I want to try some different values here.
-    
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
         x = F.relu(x)
@@ -28,4 +30,4 @@ class GNN(nn.Module):
         x = self.conv5(x, edge_index)
         x = F.relu(x)
         x = self.conv6(x, edge_index)
-        return F.log_softmax(x, dim=1) # Fiddle with this.
+        return F.log_softmax(x, dim=1)
