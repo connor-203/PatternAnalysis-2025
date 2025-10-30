68e024fa3e30d22d45478abb45d08781c1739670
connor-203 <chthewilliam2357@gmail.com>
2025-10-30 16:10:03 +1000

    Update predict.py

--- recognition/47451979-FacebookGNN/predict.py
+++ recognition/47451979-FacebookGNN/predict.py
@@ -7,24 +7,29 @@ import umap.plot
 
 
 # This little block of code is also based off the code from medium.com. Any port in storm I suppose.
+# Though it's based off the code from medium in the same way that the action of me driving around
+# the corner is from my driving instructor.
 model.eval()
 result = model(data.x, data.edge_index)
 pred = result.argmax(dim=1) # Reduces to 1 dimension
 
+# Calculate accuracy with a list consisting of booleans representing whether
+# pred[i] is the same as data.y[i].
 test_correct = pred[test_empty] == data.y[test_empty]
 test_acc = int(test_correct.sum()) / int(test_empty.sum())
 print(f"This test had an accuracy of: {test_acc}")
 
 # For plotting all nodes
-xs = data.x
-ys = data.y
-ps = pred
+# xs = data.x
+# ys = data.y
+# ps = pred
 
 # For plotting only the test nodes.
-# xs = data.x[test_empty]
-# ys = data.y[test_empty]
-# ps = pred[test_empty]
+xs = data.x[test_empty]
+ys = data.y[test_empty]
+ps = pred[test_empty]
 
+# Somewhat clunky, but converts the category number back to the category.
 new_label = []
 for i in ys:
     if i == 0:
@@ -49,20 +54,21 @@ for i in ps:
 
 
 # UMAP doesn't have GPU support so the data must be transferred to the CPU.
+mapper = umap.UMAP().fit(xs.to('cpu'))
 
-mapperTest = umap.UMAP().fit(xs.to('cpu'))
+# Plot true labels,
+plotted = umap.plot.points(mapper, labels=np.array(new_label))
+plt.savefig("assets/TrueLabels.png")
 
-plotted = umap.plot.points(mapperTest, labels=np.array(new_label))
-plt.savefig("assets/True Labels.png")
-
-plottedPred = umap.plot.points(mapperTest, labels=np.array(pred_label))
-plt.savefig("assets/Predicted Labels.png")
+# Plot predicted labels.
+plottedPred = umap.plot.points(mapper, labels=np.array(pred_label))
+plt.savefig("assets/PredictedLabels.png")
 
 # connect = umap.plot.connectivity(mapper, show_points=True)
 
 # Hammer_bundle operation requires dask and scikit-image
-connect = umap.plot.connectivity(mapperTest, edge_bundling='hammer')
-plt.savefig("assets/Recent Hammer.png")
+connect = umap.plot.connectivity(mapper, edge_bundling='hammer')
+plt.savefig("assets/HammerTest.png")
 
 plt.show()
 # umap.plot.show(plotted)
