d313170819a75426a144d061d11f0e85335073d1
connor-203 <chthewilliam2357@gmail.com>
2025-10-30 16:11:21 +1000

    Update train.py with comments

--- recognition/47451979-FacebookGNN/train.py
+++ recognition/47451979-FacebookGNN/train.py
@@ -1,4 +1,4 @@
-from dataset import data, train_empty, test_empty, device
+from dataset import data, train_empty, test_empty, val_empty, device
 from modules import GNN
 import matplotlib.pyplot as plt
 import torch
@@ -10,9 +10,7 @@ import torch
 # Whether you want to see graphs at the end.
 plotting = True
 
-# This bit of code is fairly closely based on the code from medium, I
-# have suspicions though that medium got it from Kaggle. In any case,
-# this exact block of code is used for quite a few machine learning problems.
+# Setting up model, loss function, and optimiser.
 model = GNN().to(device)
 criterion = torch.nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
@@ -22,25 +20,31 @@ losses_train = []
 losses_val = []
 
 for epoch in range(2000):
+    # Training block.
     model.train()
-    optimizer.zero_grad()
-    pred = model(data.x, data.edge_index)
-    loss = criterion(pred[train_empty], data.y[train_empty])
+    optimizer.zero_grad() # Zero the gradients.
+    pred = model(data.x, data.edge_index) # Predict on all nodes.
+    loss = criterion(pred[train_empty], data.y[train_empty]) # Look only at train nodes for error.
     loss.backward()
     optimizer.step()
 
+    # Append epoch number and training loss.
     times.append(epoch)
     losses_train.append(loss.item())
 
-    # Calculate validation loss. Code loosely based on something found on stackoverflow.
+    # Validation block.
     model.eval()
-    pred_val = model(data.x, data.edge_index)
-    loss_val = criterion(pred_val[test_empty], data.y[test_empty])
+    pred_val = model(data.x, data.edge_index) # Predict on all nodes.
+    loss_val = criterion(pred_val[val_empty], data.y[val_empty]) # Look only at validation nodes.
+
+    # Append validation loss.
     losses_val.append(loss_val.item())
 
+    # Print training and validation loss.
     if (epoch+1) % 100 == 0:
         print(f"Epoch: {epoch + 1:03d}, Training Loss: {loss:.4f}, Validation Loss: {loss_val:.4f}")
 
+# Plot training and validation loss.
 plt.plot(times, losses_train, label="Training Loss")
 plt.plot(times, losses_val, label="Validation Loss")
 plt.title("Training and Validation Losses")
